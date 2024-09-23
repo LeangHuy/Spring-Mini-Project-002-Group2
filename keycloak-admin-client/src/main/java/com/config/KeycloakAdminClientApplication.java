4dd8d2b20b2c21295c5fb6498bd2404081931628
LeangHuy <120023658+LeangHuy@users.noreply.github.com>
2024-09-23 12:19:18 +0700

    config gateway swagger and keycloak-admin-client done

--- keycloak-admin-client/src/main/java/com/config/KeycloakAdminClientApplication.java
+++ keycloak-admin-client/src/main/java/com/config/KeycloakAdminClientApplication.java
@@ -1,8 +1,14 @@
 package com.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,8 +20,18 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
         info = @Info(title = "Keycloak Admin Client",
         version = "v1",
         description = "Keycloak Admin Client"))
+@SecurityScheme(
+        name = "mini-project-2",
+        type = SecuritySchemeType.OAUTH2,
+        flows = @OAuthFlows(
+                clientCredentials = @OAuthFlow(
+                        tokenUrl = "http://localhost:8080/realms/mini-project-2/protocol/openid-connect/token"
+                )
+        )
+)
 public class KeycloakAdminClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(KeycloakAdminClientApplication.class, args);
     }
+
 }
